17034e7b1f74502e1b0d8fcbbabefed7004e5bc5
Alfredo Martinez Garcia <psialfredomtz@gmail.com>
2021-02-22 22:55:07 -0600

    pruebas y ejecucion de codigos

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -6,10 +6,10 @@ const ten = 10;
 test('Probar string', () => {
     expect(text).toMatch(/hello/);
 })
-test('Existe una fresa?', ()=> {
+test('Existe una fresa?', () => {
     expect(fruits).toContain('fresa');
 })
-test ('El numero es mayor que: ', () => {
+test('El numero es mayor que: ', () => {
     expect(ten).toBeGreaterThan(7);
 })
 test('es un boleano', () => {
@@ -18,8 +18,46 @@ test('es un boleano', () => {
 const reverseString = (str, callback) => {
     callback(str.split("").reverse().join(""))
 }
-test('should be callback', ()=>{
-    reverseString('porfirio', (str)=>{
+test('should be callback', () => {
+    reverseString('porfirio', (str) => {
         expect(str).toBe('oirifrop')
     })
 })
+
+const reverseString2 = str => {
+    return new Promise((resolve, reject) => {
+        if (!str) {
+            reject(Error('Esto es un Error '))
+        }
+        resolve(str.split("").reverse().join(""))
+    })
+}
+
+test('should promise', () => {
+    return (reverseString2('Hola'))
+        .then(str => {
+            expect(str).toBe('aloH');
+        })
+})
+test('should async/await', async () => {
+    const string = await reverseString2('Hola'); 
+    expect (string).toBe('aloH')
+})
+
+// correr código despues de cada prueba
+
+// afterEach(()=>{
+//     console.log('Después de cada prueba');
+// })
+
+// afterAll(() => { 
+//     console.log('Después de todas las pruebas');
+// })
+
+// beforeEach(() => {
+//     console.log('Antes de cada prueba');
+// })
+
+// beforeAll(() => {
+//     console.log('Antes de todas las prueba');
+// })
